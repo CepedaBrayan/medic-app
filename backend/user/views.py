d9e2ae7a79f93ed6a6f57139fdd5ae52cc41d81e
Brayan Rivera Cepeda <riveracepedabrayan@gmail.com>
2023-02-14 11:56:48 -0500

    pre-commits ready

--- backend/user/views.py
+++ backend/user/views.py
@@ -55,3 +55,8 @@ class userViewSet(viewsets.ModelViewSet):
             serializer.save()
             return HttpResponse(status=204)
         return HttpResponse(serializer.errors, status=400)
+
+    # endpoint for say hello
+    @action(detail=False, methods=["get"])
+    def hello(self, request):
+        return HttpResponse("Hello")
